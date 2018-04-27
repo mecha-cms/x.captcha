6c2176dda5897b5876210fad60af7a39f13a4593
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-04-27 17:37:39 +0700

    Remove Captcha Toggle (Simply Use Captcha Token)

--- captcha/lot/worker/captcha.token.php
+++ captcha/lot/worker/captcha.token.php
@@ -6,7 +6,7 @@ list($id, $hash, $html) = array_replace([null, null, true], $lot);
 if ($hash === null) {
     $hash = Guardian::hash();
 } else if (is_callable($hash)) {
-    $hash = $hash(Guardian::hash());
+    $hash = call_user_func($hash, Guardian::hash());
 }
 
 Captcha::set($id, $hash);
