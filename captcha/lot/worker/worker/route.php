7263a9dcc958dbac406e69a908b54a579f20e5b2
Taufik Nurrohman <t.nurrohman77@gmail.com>
2018-10-07 07:18:54 +0700

    Namespace(s)

--- captcha/lot/worker/worker/route.php
+++ captcha/lot/worker/worker/route.php
@@ -1,6 +1,6 @@
-<?php
+<?php namespace fn\captcha;
 
-function fn_captcha_text_color($hex) {
+function color($hex) {
     if (is_string($hex) && preg_match('#[a-f\d]{3,6}#i', $hex, $m)) {
         $color = $m[0];
         if (strlen($color) !== 3 && strlen($color) !== 6) {
@@ -14,26 +14,26 @@ function fn_captcha_text_color($hex) {
     return [];
 }
 
-Route::set('captcha.png', function() {
-    HTTP::type('image/png')->header([
+\Route::set('captcha.png', function() {
+    \HTTP::type('image/png')->header([
         'Cache-Control' => 'no-store, no-cache, must-revalidate, max-age=0',
         'Cache-Control' => 'post-check=0, pre-check=0',
         'Pragma' => 'no-cache'
     ]);
-    $id = HTTP::get('id');
-    extract(To::anemon(Captcha::get($id . '_')));
-    $text = Captcha::get($id);
+    $id = \HTTP::get('id');
+    extract(\To::anemon(\Captcha::get($id . '_')));
+    $text = \Captcha::get($id);
     $image = imagecreatetruecolor($w, $h);
-    $b = fn_captcha_text_color($b) ?: [255, 255, 255, 0];
-    $c = fn_captcha_text_color($c) ?: [0, 0, 0, 1];
-    $f = Path::D(__DIR__, 2) . DS . 'asset' . DS . 'ttf' . DS . $f . '.ttf';
+    $b = color($b) ?: [255, 255, 255, 0];
+    $c = color($c) ?: [0, 0, 0, 1];
+    $f = \Path::D(__DIR__, 2) . DS . 'asset' . DS . 'ttf' . DS . $f . '.ttf';
     imagefill($image, 0, 0, 0x7fff0000);
     imagealphablending($image, true);
     imagesavealpha($image, true);
     $b = imagecolorallocatealpha($image, $b[0], $b[1], $b[2], 127 - ($b[3] * 127));
     $c = imagecolorallocatealpha($image, $c[0], $c[1], $c[2], 127 - ($c[3] * 127));
     imagefilledrectangle($image, 0, 0, $w, $h, $b);
-    // center the image text…
+    // Center the image text…
     $xi = imagesx($image);
     $yi = imagesy($image);
     $box = imagettfbbox($s, 0, $f, $text);
